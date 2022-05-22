413d671adef5268bebb1d2968ce7fbe40a238c62
ashish9patel <37448871+ashish9patel@users.noreply.github.com>
2022-05-23 00:39:45 +0530

    implemented nested view,moved controls from App.view to PO.view

--- FirstApp/controller/App.controller.js
+++ FirstApp/controller/App.controller.js
@@ -1,23 +1,24 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    'sap/m/MessageToast',
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
- ], function (Controller,MessageToast,JSONModel,ResourceModel) {
+    "sap/ui/core/mvc/Controller"
+    // ,
+    // 'sap/m/MessageToast',
+    // "sap/ui/model/json/JSONModel",
+    // "sap/ui/model/resource/ResourceModel"
+ ], function (Controller) {
     "use strict";
     return Controller.extend("akp.po.walkthrough.controller.App", {
       // onInit: function () {
       // },
 
-      onSearchPO: function (evt) {
-        // MessageToast.show("searching POs...")
-        // MessageToast.show(evt);  //show hardcoded message recieved from view
+      // onSearchPO: function (evt) {
+      //   // MessageToast.show("searching POs...")
+      //   // MessageToast.show(evt);  //show hardcoded message recieved from view
 
-        //show i18n message with parameters.
-        var oBundle = this.getView().getModel("i18n").getResourceBundle();
-        var sMsg = oBundle.getText("msgWithParamerters",["X", "Y"]);
-        MessageToast.show(sMsg);
-      }
+      //   //show i18n message with parameters.
+      //   var oBundle = this.getView().getModel("i18n").getResourceBundle();
+      //   var sMsg = oBundle.getText("msgWithParamerters",["X", "Y"]);
+      //   MessageToast.show(sMsg);
+      // }
 
     });
  });
