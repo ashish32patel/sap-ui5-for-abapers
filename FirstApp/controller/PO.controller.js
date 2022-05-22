48a3d0c8923885738c83a25154b4c7458d986928
ashish9patel <37448871+ashish9patel@users.noreply.github.com>
2022-05-23 00:41:00 +0530

    implemented nested view,moved controls from App.view to PO.view-added missing PO view and controller

---
+++ FirstApp/controller/PO.controller.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    'sap/m/MessageToast',
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+ ], function (Controller,MessageToast,JSONModel,ResourceModel) {
+    "use strict";
+    return Controller.extend("akp.po.walkthrough.controller.PO", {
+      // onInit: function () {
+      // },
+
+      onSearchPO: function (evt) {
+        // MessageToast.show("searching POs...")
+        // MessageToast.show(evt);  //show hardcoded message recieved from view
+
+        //show i18n message with parameters.
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        var sMsg = oBundle.getText("msgWithParamerters",["X", "Y"]);
+        MessageToast.show(sMsg);
+      }
+
+    });
+ });
